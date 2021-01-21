af4099ac9bb1ff0894c7c92eacd852444b7e91e4
Alan Griffieth <44660602+apgriffieth@users.noreply.github.com>
2021-01-20 22:00:23 -0700

    GitHub Test Alan

---
+++ CPI 441 Capstone/Assets/Scripts/GitHubTest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHubTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*******************************************
+         * Test if GitHub is working for you:
+         * Add your name to the below comments and save the project
+         * Go to GitHub Desktop and click Fetch Origin at the top
+         * In the summary field, put "GitHub Test" with your name
+         * Click commit to main
+         * Click push to origin
+         * Ask someone else to Fetch Origin and verify that they can now see your name in the comments
+         * Finished! Follow this same process anytime you make a change or add/delete anything in the project
+         * ****************************************************************************************************
+         * ****************************************************************************************************
+         * Names:
+         * Alan
+         * 
+         * 
+         * 
+         *******************************************/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
