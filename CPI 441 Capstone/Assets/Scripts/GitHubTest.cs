815a1d8fb53c74867d3c2094a8710addca70123f
Alan Griffieth <44660602+apgriffieth@users.noreply.github.com>
2021-01-20 22:44:29 -0700

    Updated Instructions

--- CPI 441 Capstone/Assets/Scripts/GitHubTest.cs
+++ CPI 441 Capstone/Assets/Scripts/GitHubTest.cs
@@ -12,16 +12,22 @@ public class GitHubTest : MonoBehaviour
          * Add your name to the below comments and save the project
          * Go to GitHub Desktop and you should see your changed files to the left
          * If any changes have been done by others, the button will change to Pull origin. Click this
-         *****************************************************************************************************
+         ******************************************************************************************************
          NOTE: If someone has been editing the same file at the same time you have and has already uploaded
          their changes, an error will pop up and you will have to click "Stash changes and continue". Once
          GitHub finishes pulling the origin, you can now click "View stash" and "Restore".
-         *****************************************************************************************************
+         ******************************************************************************************************
+         ******************************************************************************************************
+         NOTE 2: If someone has edited the exact same line(s) of code that you have been working on, a merge
+         conflict will occur. You will then have to go through each file and determine where the conflicts are
+         and either edit your file to incorporate both changes (i.e. move your code down a few lines) or just 
+         choose one file to upload (i.e. one person's changes will not be saved). In order to avoid this, it's 
+         best if only one person makes changes to a file at a time.
+         ******************************************************************************************************
          * In the summary field, put "GitHub Test" with your name
          * Click Commit to main
          * Click Push to origin at the top
          * Ask someone else to Fetch and Pull Origin and verify that they can now see your name in the comments
-         * Testing fetch origin
          * Finished! Follow this same process anytime you make a change or add/delete anything in the project
          * ****************************************************************************************************
          * ****************************************************************************************************
